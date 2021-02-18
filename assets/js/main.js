05eec0d6c79bb576373ff6f045c8c58fa353000b
JonCourbon <joncourbon@gmail.com>
2021-02-18 11:32:24 +0100

    Add shortcode for lazy loading of embedded Youtube video

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,8 @@ function switchMode(el,switchToLight,switchToDark) {
     }
     bodyClass.remove('dark');
     setTimeout(function() {
-             localStorage.setItem('lightMode','true');
-           }, 100);
+      localStorage.setItem('lightMode','true');
+    }, 100);
   }
   else{
     for(i=0;i<switchers.length;i++){
@@ -18,8 +18,8 @@ function switchMode(el,switchToLight,switchToDark) {
     }
     bodyClass.add('dark');
     setTimeout(function() {
-  						localStorage.setItem('lightMode','false');
-  					}, 100);
+      localStorage.setItem('lightMode','false');
+    }, 100);
   }
 }
 
@@ -69,3 +69,30 @@ window.addEventListener('load', (event) => {
     })
   }
 });
+
+/* Video lazy loading 
+From: https://webdesign.tutsplus.com/tutorials/how-to-lazy-load-embedded-youtube-videos--cms-26743
+*/
+prepareVideos();
+function prepareVideos(){
+  var youtube_div = document.querySelectorAll( ".youtube" );
+  for (var i = 0; i < youtube_div.length; i++) {
+    // thumbnail image source.
+    var source = "https://img.youtube.com/vi/"+ youtube_div[i].dataset.embed +"/sddefault.jpg"; 
+    // Load the image asynchronously
+    var image = new Image();
+    image.src = source;
+    image.addEventListener( "load", function() {
+      youtube_div[i].appendChild( image );
+    }(i));
+    
+    youtube_div[i].addEventListener( "click", function() {
+      var iframe = document.createElement( "iframe" );
+      iframe.setAttribute( "frameborder", "0" );
+      iframe.setAttribute( "allowfullscreen", "" );
+      iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+      this.innerHTML = "";
+      this.appendChild( iframe );
+    } );
+  }
+}
